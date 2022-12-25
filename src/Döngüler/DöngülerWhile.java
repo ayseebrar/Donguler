fcb2766b0d7161da7008cb9b96d6752c66084d2d
lenovo <lenovo@DESKTOP-ROQVQV0>
2022-12-26 02:40:59 +0300

    df

---
+++ src/Döngüler/DöngülerWhile.java
@@ -0,0 +1,43 @@
+package Döngüler;
+
+import java.util.Scanner;
+
+public class DöngülerWhile {
+
+	public static void main(String[] args) {
+		/*int i=1;
+		while(i<=5) {
+			System.out.println(i+",");
+			i++;
+			
+		}*/
+/*int i=1;
+while(i<=100) {
+	System.out.println(i);
+	i=i+2;
+}*/
+		/*int i=2;
+		while(i<=100) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+			
+			i++;
+		}
+		*/
+		int i;
+		int toplam=0;
+		Scanner input=new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("lütfen bir sayı giriniz:");
+			i= input.nextInt();
+		}
+		
+		
+		
+		
+	}
+	
+
+}
